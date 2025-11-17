09b73fa116bf6d4e4d886780e996b5c05983c037
Mar <marbou96@hotmail.com>
2025-11-17 13:23:05 +0100

    Gestion de tareas con Springboot y JavaFX

---
+++ src/main/java/com/taskmanager/task_manager/model/Task.java
@@ -0,0 +1,25 @@
+package com.taskmanager.task_manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String responsible;
+    private String status;
+}
